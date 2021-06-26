89f5c9bd99ac1351ac90c9923d47e725001528d7
Santiago Rodriguez <santiago@wincify.com>
2021-06-26 13:08:02 -0500

    feat():add qualifications area

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,6 +44,26 @@ skillsHeader.forEach((item)=>{
 })
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]')
+const tabContents = document.querySelectorAll('[data-content]')
+
+console.log(tabContents)
+console.log(tabs)
+tabs.forEach(tab=>{
+    tab.addEventListener('click',()=>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent=>{
+            tabContent.classList.remove('qualification__active');
+        })
+        target.classList.add('qualification__active');
+
+        // tabs.forEach(tab=>{
+        //     tab.classList.remove('qualification__active');
+        // })
+        // tab.classList.add('qualification__active');
+    })
+})
 
 
 /*==================== SERVICES MODAL ====================*/
