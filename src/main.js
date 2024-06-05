3e89dc432dd72ca32a411d1fe7b6827269191cd0
Santiago Rodriguez <rodrigueznohsantiagomiguel@gmail.com>
2024-06-04 20:50:32 -0600

    chore: prepare to deploy

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import './assets/main.css'
-import { createApp } from 'vue'
-import App from './App.vue'
-import FloatingVue from 'floating-vue'
-import 'floating-vue/dist/style.css'
-const app = createApp(App)
-app.use(FloatingVue)
-app.mount('#app')
